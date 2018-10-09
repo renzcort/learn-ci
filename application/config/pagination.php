00922937f6e98d391aec58d992aebb31ad147d4e
renzcort <kharenputra@gmail.com>
2018-10-09 18:09:01 +0700

    tes

---
+++ application/config/pagination.php
@@ -0,0 +1,29 @@
+<?php
+$config['base_url'] = 'url';
+$config['total_rows'] = 100;
+$config['per_page'] = 10;
+$config['uri_segment'] = 3;
+$config['num_links'] = 3;
+$config['full_tag_open'] = '<p>';
+$config['full_tag_close'] = '</p>';
+$config['first_link'] = 'First';
+$config['first_tag_open'] = '<div>';
+$config['first_tag_close'] = '</div>';
+$config['last_link'] = 'Last';
+$config['last_tag_open'] = '<div>';
+$config['last_tag_close'] = '</div>';
+$config['next_link'] = '&gt;';
+$config['next_tag_open'] = '<div>';
+$config['next_tag_close'] = '</div>';
+$config['prev_link'] = '&lt;';
+$config['prev_tag_open'] = '<div>';
+$config['prev_tag_close'] = '</div>';
+$config['cur_tag_open'] = '<b>';
+$config['cur_tag_close'] = '</b>';
+
+$config['use_page_numbers'] = TRUE;
+$config['page_query_string']  = FALSE;
+$config['reuse_query_string'] = FALSE;
+$config['prefix'] = '';
+$config['suffix'] = '';
+$config['use_global_url_suffix'] = FALSE;
